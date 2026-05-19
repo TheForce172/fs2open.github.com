e21e6d2dffc100e7ec65f6778fa638c98c577b8c
The Force <2040992+TheForce172@users.noreply.github.com>
2026-05-19 19:44:50 +0100

    don't try to turn help file into appimage

--- ci/linux/generate_appimage.sh
+++ ci/linux/generate_appimage.sh
@@ -18,7 +18,7 @@ if [ "$ENABLE_QTFRED" = "ON" ]; then
 	cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_FOLDER/qtFRED -DCOMPONENT=qtFRED -P cmake_install.cmake
 
 	# We need to be a bit creative for determining the AppImage name since we don't want to hard-code the name
-	FILENAME="$(find $INSTALL_FOLDER/qtFRED/bin -iname 'qtfred_*' -type f -printf "%f\n").AppImage"
+	FILENAME="$(find $INSTALL_FOLDER/qtFRED/bin -iname 'qtfred_*' ! -iname '*help*' -type f -printf "%f\n").AppImage"
 	appimagetool -n "$INSTALL_FOLDER/qtFRED" "$INSTALL_FOLDER/$FILENAME"
 	chmod +x "$INSTALL_FOLDER/$FILENAME"
 fi
